d88bf5124a623f36553645d9177d988d599eda13
Kevin Hudgens <hudgens1073@gmail.com>
2021-10-29 20:34:17 +0900

    encapsulated the clock struct class

--- chapter2/FullyFeaturedClasses/methods.cpp
+++ chapter2/FullyFeaturedClasses/methods.cpp
@@ -2,21 +2,33 @@
 
 struct ClockOfTheLongNow
 {
+    // access modifier makes the year method privat
+private:
     int year;
+
     void add_year()
     {
         year++;
     };
+
+    // Getter and setter methods
+
+    bool set_year(int new_year)
+    {
+        // When setting the new year if the user enters anything less than 2019, it break
+        if (new_year < 2019)
+            return false;
+
+        year = new_year;
+        return true;
+    }
+
+    int get_year()
+    {
+        return year;
+    }
 };
 
 int main()
 {
-    ClockOfTheLongNow clock;
-    clock.year = 2021;
-    printf("The year is %d\n", clock.year);
-
-    clock.add_year();
-    printf("The year is %d\n", clock.year);
-    clock.add_year();
-    printf("The year is %d\n", clock.year);
 }
