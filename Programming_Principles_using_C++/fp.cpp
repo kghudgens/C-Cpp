23238ce1d0b16e2637a066f049cc1cf6ff509e27
Kevin Hudgens <hudgens1073@gmail.com>
2021-10-30 22:21:24 +0900

    understanding header files, hello world

---
+++ Programming_Principles_using_C++/fp.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello, programming!\n";
+    cout << "Here we go!";
+    return 0;
+};
