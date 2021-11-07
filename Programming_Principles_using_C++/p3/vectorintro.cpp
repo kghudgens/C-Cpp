1a79776ed2b1bbe9bdd09e44613dadf75932cb7f
Kevin Hudgens <hudgens1073@gmail.com>
2021-11-07 22:32:46 +0900

    Created vectors

---
+++ Programming_Principles_using_C++/p3/vectorintro.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+int main()
+{
+    std::vector<int> v(6);
+    v[0] = 5;
+    v[1] = 7;
+    v[2] = 10;
+    v[3] = 9;
+    v[4] = 6;
+    v[5] = 8;
+
+    std::vector<std::string> philospher(4);
+
+    philospher[0] = "Kant";
+    philospher[1] = "Plato";
+    philospher[2] = "Hume";
+    philospher[3] = "Kierkgaard";
+}
