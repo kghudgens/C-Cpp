a76da3c523273ac2c312ab7dea8832455b4d6d68
Kevin Hudgens <hudgens1073@gmail.com>
2021-11-28 22:41:37 +0900

    created token class

--- Programming_Principles_using_C++/p6/calculator1.cpp
+++ Programming_Principles_using_C++/p6/calculator1.cpp
@@ -10,6 +10,17 @@ void error(std::string message)
     std::cout << message;
 }
 
+class Token
+{
+public:
+    char kind;
+    double value;
+    Token(char ch)
+        : kind(ch), value(0) {}
+    Token(char ch, double val)
+        : kind(ch), value(val) {}
+};
+
 int main()
 {
     std::cout << "Please enter expression (we can handle +, *, -): \n";
