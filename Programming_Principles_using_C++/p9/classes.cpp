a8d38078259eca41053156f3017485f95fa8430d
Kevin Hudgens <hudgens1073@gmail.com>
2021-12-23 15:10:11 +0900

    review of dot notation

--- Programming_Principles_using_C++/p9/classes.cpp
+++ Programming_Principles_using_C++/p9/classes.cpp
@@ -10,3 +10,11 @@ public:    // Access modifier
         return old;
     } // Function member
 };
+
+// access members through .notation object.member
+int main()
+{
+    X var;             // var is a variable of type X
+    var.m = 7;         // assign to var's data member m
+    int x = var.mf(9); // call vars member function
+}
