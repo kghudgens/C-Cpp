82cda481b65ffd9cb31010cf4670a0e9a27c1014
Kevin Hudgens <hudgens1073@gmail.com>
2021-12-23 15:16:49 +0900

    difference between interface and implementation

---
+++ Programming_Principles_using_C++/p9/interfaceVimplementation.cpp
@@ -0,0 +1,15 @@
+class X
+{
+public:
+    // public members
+    //   - the interface to users (accessible by all)
+    // functions
+    // types
+    // data (should be private but can be public)
+private:
+    // private members
+    //   - the implementation details (used by the members of this class only)
+    // function
+    // types
+    //  data
+};
