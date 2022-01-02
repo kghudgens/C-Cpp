0310ef8d58fc9ed310b00ad1a0973874ebac7e11
Kevin Hudgens <hudgens1073@gmail.com>
2022-01-02 10:11:34 +0900

    throwing exception if inpt is incorrect

--- Programming_Principles_using_C++/p9/dateEncapsulation.cpp
+++ Programming_Principles_using_C++/p9/dateEncapsulation.cpp
@@ -4,18 +4,24 @@
 class Date
 {
     int y, m, d;
+    bool check(); // return true if date is valid
 
 public:
+    class Invalid
+    {
+    }; // to be used as an exception
     Date(int y, int m, int d);
     void add_day(int n);
     int month() { return m; };
-    int day();
-    int year();
+    int day() { return d; };
+    int year() { return y; };
 };
 
 Date::Date(int yy, int mm, int dd)
     : y(yy), m(mm), d(dd)
 {
+    if (!check())
+        throw Invalid();
 }
 
 void Date::add_day(int n)
@@ -26,6 +32,17 @@ void Date::add_day(int n)
 void f(Date d1, Date d2)
 {
     std::cout << d1.month() << " " << d2.month() << "\n";
+}
+
+bool Date::check()
+{
+    if (m < 1 || 12 < m)
+        return false;
+    if (d < 1 || 31 < d)
+        return false;
+    if (y < 2022 || 2022 < y)
+        return false;
+    return true;
 }
 
 int main()
