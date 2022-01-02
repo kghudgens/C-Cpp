07f52bee4540ed8ed0fd0ca0f0b2ebd26f13e66c
Kevin Hudgens <hudgens1073@gmail.com>
2022-01-02 10:05:29 +0900

    passing objects as arguments

--- Programming_Principles_using_C++/p9/dateEncapsulation.cpp
+++ Programming_Principles_using_C++/p9/dateEncapsulation.cpp
@@ -1,3 +1,6 @@
+#include <iostream>
+#include <string>
+
 class Date
 {
     int y, m, d;
@@ -5,7 +8,29 @@ class Date
 public:
     Date(int y, int m, int d);
     void add_day(int n);
-    int month() { return m; }
-    int day() { return d; }
-    int year() { return y; }
+    int month() { return m; };
+    int day();
+    int year();
 };
+
+Date::Date(int yy, int mm, int dd)
+    : y(yy), m(mm), d(dd)
+{
+}
+
+void Date::add_day(int n)
+{
+    d = d + n;
+}
+
+void f(Date d1, Date d2)
+{
+    std::cout << d1.month() << " " << d2.month() << "\n";
+}
+
+int main()
+{
+    Date d1(2022, 01, 02);
+    Date d2(2022, 02, 01);
+    f(d1, d2);
+}
