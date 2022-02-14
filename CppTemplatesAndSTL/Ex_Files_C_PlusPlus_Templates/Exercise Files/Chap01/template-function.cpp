e12b29309f39a6f5fc786c65501e110b25db173d
Kevin Hudgens <hudgens1073@gmail.com>
2022-02-14 16:41:27 +0900

    templates explained

--- CppTemplatesAndSTL/Ex_Files_C_PlusPlus_Templates/Exercise Files/Chap01/template-function.cpp
+++ CppTemplatesAndSTL/Ex_Files_C_PlusPlus_Templates/Exercise Files/Chap01/template-function.cpp
@@ -5,15 +5,24 @@
 using namespace std;
 
 template <typename T>
-T maxof ( const T & a, const T & b ) {
-    return ( a > b ? a : b );
+T maxof(const T &a, const T &b)
+{
+    return (a > b ? a : b);
 }
+// Once the compiler gets to the method call in the  main function, the
+// compiler creates the below method as a place holder and performs its operations
+// with its newly found data type
+// int maxof(const int &a, const int &b)
+// {
+//     return (a > b ? a : b);
+// }
 
-int main() {
+int main()
+{
     int a = 7;
     int b = 9;
-    
-    cout << "max is " << maxof<int>( a, b ) << endl;
-    
+
+    cout << "max is " << maxof<int>(a, b) << endl;
+
     return 0;
 }
